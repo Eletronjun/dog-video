47264bac9bc3365b78637a9ed6e533920926c3ba
Christopher Paraizo <wanjo.christopher@gmail.com>
2026-07-22 12:27:00 -0300

    fix(test): desativa coverage automatico do jest para evitar erro de EACCES no container Docker

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     testEnvironment: 'node', // Define o ambiente de teste como Node.js
     testMatch: ['**/tests/**/*.test.js'], // Padrão para encontrar arquivos de teste
-    collectCoverage: true, // Habilita a cobertura de código
+    collectCoverage: false, // O coverage será gerado apenas ao rodar `make test-coverage`
     coverageDirectory: 'coverage', // Pasta onde os relatórios de cobertura serão salvos
   };
